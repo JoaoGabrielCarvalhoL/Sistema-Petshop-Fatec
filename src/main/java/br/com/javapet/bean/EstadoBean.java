d758d3fba5d54431289587c859b94710b5d7a312
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-10-21 20:54:59 -0300

    Atulizando EstadoBean com OmniFaces

--- src/main/java/br/com/javapet/bean/EstadoBean.java
+++ src/main/java/br/com/javapet/bean/EstadoBean.java
@@ -1,18 +1,14 @@
 package br.com.javapet.bean;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
 
 @ManagedBean
 public class EstadoBean 
 {
 	public void salvar()
 	{
-		String texto = "Teste";
-		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
-		
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, mensagem);
+		Messages.addGlobalInfo("Teste sistema Petshop");
 	}
 }
