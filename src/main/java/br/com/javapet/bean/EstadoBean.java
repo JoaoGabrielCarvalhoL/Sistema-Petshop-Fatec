db2e729acf27fd3c173508a075300132115a1225
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-10-21 22:29:55 -0300

    EstadoBean Salvar

--- src/main/java/br/com/javapet/bean/EstadoBean.java
+++ src/main/java/br/com/javapet/bean/EstadoBean.java
@@ -1,14 +1,51 @@
 package br.com.javapet.bean;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.javapet.dao.EstadoDao;
+import br.com.javapet.domain.Estado;
+
+@SuppressWarnings("serial")
 @ManagedBean
-public class EstadoBean 
+@ViewScoped
+public class EstadoBean implements Serializable
 {
+	private Estado estado;
+	
+	public Estado getEstado() 
+	{
+		return estado;
+	}
+	
+	public void setEstado(Estado estado) 
+	{
+		this.estado = estado;
+	}
+	
+	
 	public void salvar()
 	{
-		Messages.addGlobalInfo("Teste sistema Petshop");
+		try
+		{
+		EstadoDao estadoDao = new EstadoDao();
+		estadoDao.salvar(estado);
+		novo();
+		Messages.addGlobalInfo("Estado salvo com sucesso!");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Erro ao tentar salvar o Estado");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void novo() 
+	{
+		estado = new Estado(); 
 	}
 }
