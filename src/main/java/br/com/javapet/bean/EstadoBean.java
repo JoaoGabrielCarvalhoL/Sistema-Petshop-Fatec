2e4bc7e250c7d0a83d5d53a93bea2593cb729a6b
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-10-23 00:06:58 -0300

    EstadoBean - Excluir

--- src/main/java/br/com/javapet/bean/EstadoBean.java
+++ src/main/java/br/com/javapet/bean/EstadoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -45,10 +46,11 @@ public class EstadoBean implements Serializable
 	{
 		try
 		{
-		EstadoDao estadoDao = new EstadoDao();
-		estadoDao.salvar(estado);
-		novo();
-		Messages.addGlobalInfo("Estado salvo com sucesso!");
+			EstadoDao estadoDao = new EstadoDao();
+			estadoDao.salvar(estado);
+			novo();
+			estados = estadoDao.listar();
+			Messages.addGlobalInfo("Estado salvo com sucesso!");
 		}
 		catch(RuntimeException erro)
 		{
@@ -56,6 +58,24 @@ public class EstadoBean implements Serializable
 			erro.printStackTrace();
 		}
 	}
+	
+	public void excluir(ActionEvent evento)
+	{
+		try 
+		{
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+			EstadoDao estadoDao = new EstadoDao(); 
+			estadoDao.excluir(estado);
+			estados = estadoDao.listar();
+			Messages.addGlobalInfo("Estado removido com sucesso!");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar excluir o estado!");
+			erro.printStackTrace();
+		}
+	}
+		
 	
 	@PostConstruct
 	public void listar()
