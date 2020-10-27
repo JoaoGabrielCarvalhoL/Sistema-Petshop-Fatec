b6ec6e256f4175b3860a37841e4aa28e56c4a7c4
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-10-27 20:56:15 -0300

    ClienteBean Novo

--- src/main/java/br/com/javapet/bean/ClienteBean.java
+++ src/main/java/br/com/javapet/bean/ClienteBean.java
@@ -10,7 +10,9 @@ import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
 import br.com.javapet.dao.ClienteDao;
+import br.com.javapet.dao.PessoaDao;
 import br.com.javapet.domain.Cliente;
+import br.com.javapet.domain.Pessoa;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -20,6 +22,8 @@ public class ClienteBean implements Serializable
 	private Cliente cliente;
 	
 	private List<Cliente> clientes; 
+	
+	private List<Pessoa> pessoas; 
 	
 	
 	public List<Cliente> getClientes() 
@@ -41,6 +45,16 @@ public class ClienteBean implements Serializable
 	{
 		this.cliente = cliente;
 	}
+	
+	public List<Pessoa> getPessoas() 
+	{
+		return pessoas;
+	}
+	
+	public void setPessoas(List<Pessoa> pessoas) 
+	{
+		this.pessoas = pessoas;
+	}
 
 	@PostConstruct
 	public void listar()
@@ -52,7 +66,23 @@ public class ClienteBean implements Serializable
 		}
 		catch(RuntimeException erro)
 		{
-			Messages.addGlobalError("Ocorreu um erro ao tentar listar clientes");
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar clientes.");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void novo()
+	{
+		try
+		{
+			cliente = new Cliente(); 
+			PessoaDao pessoaDao = new PessoaDao();
+		
+			pessoas = pessoaDao.listar("nome");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo cliente.");
 			erro.printStackTrace();
 		}
 	}
