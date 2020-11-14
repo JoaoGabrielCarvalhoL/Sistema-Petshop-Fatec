4806d303dffeb5fe2e45b44dd49973fc7ac375ac
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-11-14 14:36:58 -0300

    FuncionarioBean e View - Completo

---
+++ src/main/java/br/com/javapet/bean/AdministradorBean.java
@@ -0,0 +1,5 @@
+package br.com.javapet.bean;
+
+public class AdministradorBean {
+
+}
---
+++ src/main/java/br/com/javapet/bean/FuncionarioBean.java
@@ -0,0 +1,143 @@
+package br.com.javapet.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.com.javapet.dao.FuncionarioDao;
+import br.com.javapet.dao.PessoaDao;
+import br.com.javapet.domain.Funcionario;
+import br.com.javapet.domain.Pessoa;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class FuncionarioBean implements Serializable
+{
+	private Funcionario funcionario; 
+	
+	private List<Funcionario> funcionarios; 
+	
+	private List<Pessoa> pessoas;
+
+	public Funcionario getFuncionario() 
+	{
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) 
+	{
+		this.funcionario = funcionario;
+	}
+
+	public List<Funcionario> getFuncionarios() 
+	{
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) 
+	{
+		this.funcionarios = funcionarios;
+	}
+
+	public List<Pessoa> getPessoas() 
+	{
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) 
+	{
+		this.pessoas = pessoas;
+	} 
+	
+	@PostConstruct
+	public void listar()
+	{
+		try
+		{
+			
+			FuncionarioDao funcionarioDao = new FuncionarioDao(); 
+			funcionarios = funcionarioDao.listar();
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar funcionários");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void novo()
+	{
+		try
+		{
+			funcionario = new Funcionario(); 
+			PessoaDao pessoaDao = new PessoaDao();
+			
+			pessoas = pessoaDao.listar("nome");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo funcionário");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void salvar() 
+	{
+		try 
+		{
+			FuncionarioDao funcionarioDao = new FuncionarioDao(); 
+			funcionarioDao.merge(funcionario);
+			
+			funcionario = new Funcionario(); 
+			funcionarios = funcionarioDao.listar("carteiraTrabalho");
+			
+			PessoaDao pessoaDao = new PessoaDao(); 
+			pessoas = pessoaDao.listar("nome");
+			Messages.addGlobalInfo("Funcionário salvo com sucesso!");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar um novo funcionário");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void excluir(ActionEvent evento)
+	{
+		try
+		{
+			funcionario = (Funcionario) evento.getComponent().getAttributes().get("funcionarioSelecionado");
+			FuncionarioDao funcionarioDao = new FuncionarioDao(); 
+			funcionarioDao.excluir(funcionario);
+			
+			funcionarios = funcionarioDao.listar("carteiraTrabalho");
+			Messages.addGlobalInfo("Funcionário excluído com sucesso!");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar excluir funcionário");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void editar(ActionEvent evento)
+	{
+		try
+		{
+			funcionario = (Funcionario) evento.getComponent().getAttributes().get("funcionarioSelecionado");
+		
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar editar funcionário");
+			erro.printStackTrace();
+		}
+	}
+}
