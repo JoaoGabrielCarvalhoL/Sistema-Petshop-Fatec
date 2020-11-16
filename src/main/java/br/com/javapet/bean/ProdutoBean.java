15f81bd86eefa6de64de46bd77a3fc309e51a004
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-11-15 23:09:29 -0300

    Implementação dos relatórios - Produtos

--- src/main/java/br/com/javapet/bean/ProdutoBean.java
+++ src/main/java/br/com/javapet/bean/ProdutoBean.java
@@ -1,17 +1,26 @@
 package br.com.javapet.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 
 import br.com.javapet.dao.ProdutoDao;
 import br.com.javapet.domain.Produto;
+import br.com.javapet.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -125,6 +134,26 @@ public class ProdutoBean implements Serializable
 		catch (RuntimeException erro)
 		{
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o produto");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void imprimir()
+	{
+		try
+		{
+			String caminho = Faces.getRealPath("/reports/Produtos.jasper");
+			
+			Map<String, Object> parametros = new HashMap<>();
+			
+			Connection conexao = HibernateUtil.getConexao();
+			
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+			JasperPrintManager.printReport(relatorio, true);
+		}
+		catch(JRException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
 			erro.printStackTrace();
 		}
 	}
--- src/main/java/br/com/javapet/util/HibernateUtil.java
+++ src/main/java/br/com/javapet/util/HibernateUtil.java
@@ -1,9 +1,13 @@
 package br.com.javapet.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.Service;
+import org.hibernate.jdbc.ReturningWork;
 import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil 
@@ -32,5 +36,21 @@ public class HibernateUtil
 			System.err.println("A fábrica de sessões não pode ser criada." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
+	}
+	
+	public static Connection getConexao()
+	{
+		Session sessao = fabricaDeSessoes.openSession();
+		Connection conexao = sessao.doReturningWork(new ReturningWork<Connection>() 
+		{
+			@Override
+			public Connection execute(Connection conn) throws SQLException 
+			{
+				// TODO Auto-generated method stub
+				return conn;
+			}
+		}); 
+		
+		return conexao; 
 	}
 }
