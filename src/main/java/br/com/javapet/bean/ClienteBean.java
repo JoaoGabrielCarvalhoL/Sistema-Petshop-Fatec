a3794fb43a77b67fdff52a9f19b0615b67d5eec6
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-11-16 21:25:30 -0300

    Implementação relatório de clientes

--- src/main/java/br/com/javapet/bean/ClienteBean.java
+++ src/main/java/br/com/javapet/bean/ClienteBean.java
@@ -1,19 +1,28 @@
 package br.com.javapet.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 
 import br.com.javapet.dao.ClienteDao;
 import br.com.javapet.dao.PessoaDao;
 import br.com.javapet.domain.Cliente;
 import br.com.javapet.domain.Pessoa;
+import br.com.javapet.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -142,6 +151,26 @@ public class ClienteBean implements Serializable
 			erro.printStackTrace();
 		}
 		
+	}
+	
+	public void imprimir()
+	{
+		try
+		{
+			String caminho = Faces.getRealPath("/reports/Produtos.jasper");
+			
+			Map<String, Object> parametros = new HashMap<>();
+			
+			Connection conexao = HibernateUtil.getConexao();
+			
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+			JasperPrintManager.printReport(relatorio, true);
+		}
+		catch(JRException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
+			erro.printStackTrace();
+		}
 	}
 	
 	
