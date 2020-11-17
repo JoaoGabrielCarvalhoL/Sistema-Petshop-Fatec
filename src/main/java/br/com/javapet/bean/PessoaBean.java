3d088bbae866b418696a5c1b92df79c8dc61a23a
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-11-16 21:29:56 -0300

    Implementação relatório pessoas

--- src/main/java/br/com/javapet/bean/PessoaBean.java
+++ src/main/java/br/com/javapet/bean/PessoaBean.java
@@ -1,14 +1,18 @@
 package br.com.javapet.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 
 import br.com.javapet.dao.CidadeDao;
@@ -17,6 +21,11 @@ import br.com.javapet.dao.PessoaDao;
 import br.com.javapet.domain.Cidade;
 import br.com.javapet.domain.Estado;
 import br.com.javapet.domain.Pessoa;
+import br.com.javapet.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -196,6 +205,26 @@ public class PessoaBean implements Serializable
 		catch(RuntimeException erro)
 		{
 			Messages.addGlobalError("Ocorre um erro ao tentar filtrar as cidades");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void imprimir()
+	{
+		try
+		{
+			String caminho = Faces.getRealPath("/reports/Produtos.jasper");
+			
+			Map<String, Object> parametros = new HashMap<>();
+			
+			Connection conexao = HibernateUtil.getConexao();
+			
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+			JasperPrintManager.printReport(relatorio, true);
+		}
+		catch(JRException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
 			erro.printStackTrace();
 		}
 	}
