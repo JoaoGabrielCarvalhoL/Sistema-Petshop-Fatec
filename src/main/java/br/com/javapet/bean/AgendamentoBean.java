92ceda189744756dfb8965a02f43a2d5003d29fb
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-11-18 21:56:20 -0300

    Finalização do Projeto MeuPet - Sistema de Gerenciamento para Pethops

--- src/main/java/br/com/javapet/bean/AgendamentoBean.java
+++ src/main/java/br/com/javapet/bean/AgendamentoBean.java
@@ -1,17 +1,14 @@
 package br.com.javapet.bean;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
-import org.primefaces.event.SelectEvent;
-import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.ScheduleModel;
 
 import br.com.javapet.dao.AgendamentoDao;
 import br.com.javapet.dao.ClienteDao;
@@ -20,19 +17,25 @@ import br.com.javapet.domain.Agendamento;
 import br.com.javapet.domain.Cliente;
 import br.com.javapet.domain.Produto;
 
+@SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped 
-public class AgendamentoBean 
+public class AgendamentoBean implements Serializable
 {
 	private Agendamento agendamento; 
-	
-	private ScheduleModel agendamentos; 
 	
 	private List<Cliente> clientes; 
 	
 	private List<Produto> produtos; 
 	
-	public Agendamento getAgendamento() 
+	private List<Agendamento> agendamentos;
+	
+	private Produto produto; 
+	
+	private Cliente cliente;
+
+
+	public Agendamento getAgendamento()
 	{
 		return agendamento;
 	}
@@ -52,66 +55,141 @@ public class AgendamentoBean
 		this.clientes = clientes;
 	}
 
-	public List<Produto> getProdutos() {
+	public List<Produto> getProdutos()
+	{
 		return produtos;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
+	public void setProdutos(List<Produto> produtos) 
+	{
 		this.produtos = produtos;
 	}
-	
-	public ScheduleModel getAgendamentos() 
+
+	public List<Agendamento> getAgendamentos()
 	{
 		return agendamentos;
 	}
-	
-	public void setAgendamentos(ScheduleModel agendamentos) 
+
+	public void setAgendamentos(List<Agendamento> agendamentos) 
 	{
 		this.agendamentos = agendamentos;
 	}
 	
-	
-	
-	
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
 	@PostConstruct
 	public void listar()
 	{
-		agendamentos = new DefaultScheduleModel();
+		try
+		{
+			AgendamentoDao agendamentoDao = new AgendamentoDao(); 
+			agendamentos = agendamentoDao.listar("dataAgendamento");
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar agendamentos");
+			erro.printStackTrace();
+		}
 	}
 	
-	
-	public void novo(SelectEvent evento)
+	public void novo()
 	{
-		agendamento = new Agendamento(); 
+		try
+		{
+			agendamento = new Agendamento();
+			cliente = new Cliente(); 
+			produto = new Produto(); 
+			
+			
+			ClienteDao clienteDao = new ClienteDao(); 
+			clientes = clienteDao.listar();
+			
+			ProdutoDao produtoDao = new ProdutoDao(); 
+			produtos = produtoDao.listar();
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo agendamento");
+			erro.printStackTrace();
+		}
 		
-		agendamento.setDataAgendamento((Date) evento.getObject());
-		
-		ClienteDao clienteDao = new ClienteDao(); 
-		clientes = clienteDao.listar();
-		
-		ProdutoDao produtoDao = new ProdutoDao(); 
-		produtos = produtoDao.listar();
-		 
 	}
 	
 	public void salvar()
 	{
-		Calendar calendar = Calendar.getInstance(); 
-		calendar.setTime(agendamento.getDataAgendamento());
-		calendar.add(Calendar.DATE, 1);
-		agendamento.setDataAgendamento(calendar.getTime());
-		
 		try
 		{
 			AgendamentoDao agendamentoDao = new AgendamentoDao(); 
-			agendamentoDao.salvar(agendamento);
+			agendamentoDao.merge(agendamento);
+			
+			agendamento = new Agendamento(); 
+			agendamentos = agendamentoDao.listar("dataAgendamento");
+			
+			ClienteDao clienteDao = new ClienteDao(); 
+			clientes = clienteDao.listar(); 
+			
+			ProdutoDao produtoDao = new ProdutoDao();
+			produtos = produtoDao.listar();
+			
 			Messages.addGlobalInfo("Agendamento salvo com sucesso!");
 		}
 		catch(RuntimeException erro)
 		{
-			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o agendamento");
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar um novo agendamento");
 			erro.printStackTrace();
 		}
 	}
+	
+	public void excluir(ActionEvent evento)
+	{
+		try
+		{
+			agendamento = (Agendamento) evento.getComponent().getAttributes().get("agendamentoSelecionado");
+			AgendamentoDao agendamentoDao = new AgendamentoDao(); 
+			agendamentoDao.excluir(agendamento);
+			
+			agendamentos = agendamentoDao.listar("dataAgendamento");
+			Messages.addGlobalInfo("Agendamento excluído com sucesso!");
+			
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar excluir um agendamento");
+			erro.printStackTrace();
+		}
+		
+	}
+	
+	public void editar(ActionEvent evento)
+	{
+		try
+		{
+			agendamento = (Agendamento) evento.getComponent().getAttributes().get("agendamentoSelecionado");
+			
+		}
+		catch(RuntimeException erro)
+		{
+			Messages.addGlobalError("Ocorreu um erro ao tentar alterar um agendamento");
+			erro.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
-
